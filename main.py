6e4e8828d012621f0d5f004ca683fa2431cfcbc7
ShrJanLan <17873989669@163.com>
2023-08-03 11:20:33 +0800

    测试pipenv安装依赖

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+matrix = [[row[i] for row in matrix] for i in range(4)]
+print(matrix)
